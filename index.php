473fbc5ac69c32764a0c4036fede92158b211fae
daiancosta <desenvolvimentodaian@gmail.com>
2022-08-23 09:58:20 -0300

    adjust

--- index.php
+++ index.php
@@ -26,6 +26,16 @@ function getIP() {
     return $ip;
 }
 
-echo getIP();
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    My IP is: <?php echo getIP();?>
+</body>
+</html>
